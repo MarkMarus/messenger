6fa4987407276e4ba6e37b25a93e30108d7baada
Mark Marus <marusmark2@gmail.com>
2023-07-09 14:42:09 +0200

    Initial Commit

---
+++ Messanger/MessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerApp.swift
+//  Messanger
+//
+//  Created by Марк Марус on 09.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
